6aa929a96d454d07a0f680fd8222fedf5628b155
Anvil Palamattam <anvil008@me.com>
2017-04-03 23:00:14 +0300

    hyper.js font change

--- .hyper.js
+++ .hyper.js
@@ -8,7 +8,7 @@ module.exports = {
     fontSize: 11,
 
     // font family with optional fallbacks
-    fontFamily: 'Hack', 'Menlo Regular' 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'Hack, Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
