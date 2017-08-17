cae005b5f3f14229b87b59f48147a7b5f446283b
Anvil Palamattam <anvil008@me.com>
2017-08-17 19:01:43 +0300

    Update and rename of chocolately.cmd to chocolately.ps1
    Updated chocolately script for August 2017.

---
+++ windowsos/chocolately.ps1
@@ -0,0 +1,54 @@
+# Chocolately Installs
+
+# Chocolately
+cinst chocolately -y
+cinst chocolatelygui -y
+
+# Gaming
+cinst steam -y
+cinst battle.net -y
+cinst origin -y
+cinst uplay -y
+cinst gamesavemanager -y
+cinst obs-studio -y
+
+# Web
+cinst googlechrome -y
+
+# Comms
+cinst discord -y
+cinst skype -y
+cinst whatsapp -y
+
+# Productivity & Others
+cinst 7zip -y
+cinst dropbox -y
+cinst sharex -y
+cinst spotify -y
+cinst filebot -y
+cinst plexmediaserver -y
+
+# Utilities
+cinst qbittorrent -y
+cinst processhacker -y
+cinst glasswire -y
+cinst speccy -y
+
+# Developer
+cinst atom -y
+cinst git -y
+cinst github -y
+cinst jdk8 -y
+cinst silverlight -y
+cinst nodejs -y
+cinst python -y
+cinst python2 -y
+cinst adobeair -y
+
+# Currently Not Using
+# cinst malwarebytes
+# cinst hostsman
+# cinst goggalaxy
+# cinst f.lux
+# cinst autohotkey.install
+# cinst windirstat
